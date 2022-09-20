b7f5cb039e0e5a976d29b2b02d7fc69aa6e39057
mmwaikar <manoj.waikar@intercax.com>
2022-09-20 19:17:28 +0530

    Move Player to test.

--- src/main/scala/codionics/domain/Player.scala
+++
@@ -1,3 +0,0 @@
-package codionics.domain
-
-case class Player(firstName: String, lastName: String, nickName: String, city: String, country: String, zip: String)
--- src/test/scala/codionics/domain/Player.scala
+++ src/test/scala/codionics/domain/Player.scala
@@ -1,3 +1,7 @@
 package codionics.domain
 
 case class Player(firstName: String, lastName: String, nickName: String, city: String, country: String, zip: String)
+
+object Player {
+  val NULL_OBJECT: Player = Player("", "", "", "", "", "")
+}
